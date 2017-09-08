fdb1a00e4f913c3bd41728013ea3c57d22eb34a6
MinervaJeyarajah <maryannminerva@gmail.com>
2017-09-08 13:47:15 +0530

    trial

--- config/auth.js
+++ config/auth.js
@@ -6,19 +6,19 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : '1715054528790247', // your App ID
         'clientSecret'  : 'ba3abc93f10006afd294779a075392d0', // your App Secret
-        'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
+        'callbackURL'   : 'https://ukifunwork4bymino.herokuapp.com/auth/facebook/callback'
     },
 
     'twitterAuth' : {
         'consumerKey'       : 'ccA0IpQbW0JlitR4bS2IXSWbW',
         'consumerSecret'    : '3nfhfZv8bt4XyRvje2EbDiPaIhrCfqQS0GOsWNk1ryoxnWRP9S',
-        'callbackURL'       : 'http://localhost:8080/auth/twitter/callback'
+        'callbackURL'       : 'https://ukifunwork4bymino.herokuapp.com/auth/twitter/callback'
     },
 
     'googleAuth' : {
         'clientID'      : '108117548611-sj31mii7aat2ohg7d3n5nq7jmivnu90u.apps.googleusercontent.com',
         'clientSecret'  : 'UWSJyBKCNEXNiBoj3hoKWuRX',
-        'callbackURL'   : 'http://localhost:8080/auth/google/callback'
+        'callbackURL'   : 'https://ukifunwork4bymino.herokuapp.com:8080/auth/google/callback'
     }
 
 };
